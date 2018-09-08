9f76d173a84f6e15ca39bf134708b2493c98573e
SONG1279 <1183874219@qq.com>
2018-09-08 18:41:20 +0800

    test

---
+++ 100DaysOfMLCode/Day6.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('datasets/Social_Network_Ads.csv')
+X = dataset.iloc[:,[2,3]].values
+Y = dataset.iloc[:,4].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(X_train, Y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, y_pred)
+
+
+
+
+
+
